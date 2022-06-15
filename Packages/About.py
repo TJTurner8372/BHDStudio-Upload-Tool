789b188223b9fd37c43ace84733fd448665dd08a
jlw4049 <jlw_4049@hotmail.com>
2022-06-15 17:22:03 -0400

    Add TMDB endorsement string

--- Packages/About.py
+++ Packages/About.py
@@ -64,7 +64,8 @@ def openaboutwindow(main_root_title):
     about_window_text.pack()
     about_window_text.insert(INSERT, f"{main_root_title}\n")
     about_window_text.insert(INSERT, "\n")
-    about_window_text.insert(INSERT, "Development: jlw4049, thesb3")
+    about_window_text.insert(INSERT, "Development: jlw4049, thesb3\n\n\n\n\n"
+                                     "This product uses the TMDB API but is not endorsed or certified by TMDB.")
     about_window_text.configure(state=DISABLED)
 
     about_information_frame = LabelFrame(about_window, text=' License ', labelanchor="nw")
