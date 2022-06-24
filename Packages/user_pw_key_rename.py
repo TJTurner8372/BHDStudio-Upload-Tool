62edd910ee3dfec72ed963a11cae464f7a5954cb
jlw4049 <jlw_4049@hotmail.com>
2022-06-24 00:15:03 -0400

    Create user_pw_key_rename.py

---
+++ Packages/user_pw_key_rename.py
@@ -0,0 +1,9 @@
+# comment the next 3 lines of code out to generate the 'key'. Take the printed key and paste it in the crypto_key
+# from cryptography.fernet import Fernet
+# key = Fernet.generate_key() #this is your "password"
+# print(key)
+
+# key to cryptography module
+# crypto_key = b' EXAMPLE BYTE KEY'
+
+# afterwords rename this module from 'user_pw_key_rename' to 'user_pw_key'
