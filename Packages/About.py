465bb71743fb5cb4fe8c79539bb0b3e10bc94773
jlw4049 <jlw_4049@hotmail.com>
2022-08-05 16:44:54 -0400

    Update about window to handle themes

--- Packages/About.py
+++ Packages/About.py
@@ -1,20 +1,43 @@
 from configparser import ConfigParser
-from tkinter import Toplevel, INSERT, Text, DISABLED, messagebox, FLAT, N, E, W, S, font, LabelFrame
+from tkinter import (
+    Toplevel,
+    INSERT,
+    Text,
+    DISABLED,
+    messagebox,
+    FLAT,
+    N,
+    E,
+    W,
+    S,
+    font,
+    LabelFrame,
+)
 
 
 # About Window --------------------------------------------------------------------------------------------------------
-def openaboutwindow(main_root_title):
+def openaboutwindow(
+    main_root_title,
+    main_root_color,
+    frame_color_bg,
+    frame_color_fg,
+    text_color_fg,
+    text_color_bg,
+):
     global about_window
 
     # Defines the path to config.ini and opens it for reading/writing
-    config_file = 'Runtime/config.ini'  # Creates (if doesn't exist) and defines location of config.ini
+    config_file = "Runtime/config.ini"  # Creates (if doesn't exist) and defines location of config.ini
     config = ConfigParser()
     config.read(config_file)
 
     try:  # If "About" window is already opened, display a message, then close the "About" window
         if about_window.winfo_exists():
-            messagebox.showinfo(title=f'"{about_window.wm_title()}" Info!', parent=about_window,
-                                message=f'"{about_window.wm_title()}" is already opened, closing window instead')
+            messagebox.showinfo(
+                title=f'"{about_window.wm_title()}" Info!',
+                parent=about_window,
+                message=f'"{about_window.wm_title()}" is already opened, closing window instead',
+            )
             about_window.destroy()
             return
     except NameError:
@@ -23,54 +46,87 @@ def openaboutwindow(main_root_title):
     def about_exit_function():  # Exit function when hitting the 'X' button
         func_parser = ConfigParser()
         func_parser.read(config_file)
-        if about_window.wm_state() == 'normal':
-            if func_parser['save_window_locations']['about_window'] != about_window.geometry():
-                if int(about_window.geometry().split('x')[0]) >= about_window_width or \
-                        int(about_window.geometry().split('x')[1].split('+')[0]) >= about_window_height:
-                    func_parser.set('save_window_locations', 'about_window', about_window.geometry())
-                    with open(config_file, 'w') as configfile:
+        if about_window.wm_state() == "normal":
+            if (
+                func_parser["save_window_locations"]["about_window"]
+                != about_window.geometry()
+            ):
+                if (
+                    int(about_window.geometry().split("x")[0]) >= about_window_width
+                    or int(about_window.geometry().split("x")[1].split("+")[0])
+                    >= about_window_height
+                ):
+                    func_parser.set(
+                        "save_window_locations", "about_window", about_window.geometry()
+                    )
+                    with open(config_file, "w") as configfile:
                         func_parser.write(configfile)
 
         about_window.destroy()  # Close window
 
     about_window = Toplevel()
-    about_window.title('About')
-    about_window.configure(background="#434547")
+    about_window.title("About")
+    about_window.configure(background=main_root_color)
     about_window_height = 650
     about_window_width = 720
-    if config['save_window_locations']['about_window'] == '':
+    if config["save_window_locations"]["about_window"] == "":
         screen_width = about_window.winfo_screenwidth()
         screen_height = about_window.winfo_screenheight()
         x_coordinate = int((screen_width / 2) - (about_window_width / 2))
         y_coordinate = int((screen_height / 2) - (about_window_height / 2))
-        about_window.geometry("{}x{}+{}+{}".format(about_window_width, about_window_height, x_coordinate, y_coordinate))
-    elif config['save_window_locations']['about_window'] != '':
-        about_window.geometry(config['save_window_locations']['about_window'])
+        about_window.geometry(
+            "{}x{}+{}+{}".format(
+                about_window_width, about_window_height, x_coordinate, y_coordinate
+            )
+        )
+    elif config["save_window_locations"]["about_window"] != "":
+        about_window.geometry(config["save_window_locations"]["about_window"])
     about_window.resizable(False, False)
-    about_window.protocol('WM_DELETE_WINDOW', about_exit_function)
+    about_window.protocol("WM_DELETE_WINDOW", about_exit_function)
 
     about_window.grid_columnconfigure(0, weight=1)
 
-    detect_font = font.nametofont("TkDefaultFont")  # Get default font value into Font object
+    detect_font = font.nametofont(
+        "TkDefaultFont"
+    )  # Get default font value into Font object
     set_font = detect_font.actual().get("family")
 
-    about_information_frame = LabelFrame(about_window, text=' About ', labelanchor="nw")
-    about_information_frame.grid(column=0, row=0, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W)
-    about_information_frame.configure(fg="#3498db", bg="#434547", bd=3, font=(set_font, 10, "bold"))
+    about_information_frame = LabelFrame(about_window, text=" About ", labelanchor="nw")
+    about_information_frame.grid(
+        column=0, row=0, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W
+    )
+    about_information_frame.configure(
+        fg=frame_color_fg, bg=frame_color_bg, bd=3, font=(set_font, 10, "bold")
+    )
     about_information_frame.grid_rowconfigure(0, weight=1)
     about_information_frame.grid_columnconfigure(0, weight=1)
 
-    about_window_text = Text(about_information_frame, background="#434547", foreground="white", relief=FLAT, height=10)
+    about_window_text = Text(
+        about_information_frame,
+        background=text_color_bg,
+        foreground=text_color_fg,
+        relief=FLAT,
+        height=10,
+    )
     about_window_text.pack()
     about_window_text.insert(INSERT, f"{main_root_title}\n")
     about_window_text.insert(INSERT, "\n")
-    about_window_text.insert(INSERT, "Development: jlw4049, thesb3\n\n\n\n\n"
-                                     "This product uses the TMDB API but is not endorsed or certified by TMDB.")
+    about_window_text.insert(
+        INSERT,
+        "Development: jlw4049, thesb3\n\n\n\n\n"
+        "This product uses the TMDB API but is not endorsed or certified by TMDB.",
+    )
     about_window_text.configure(state=DISABLED)
 
-    about_information_frame = LabelFrame(about_window, text=' License ', labelanchor="nw")
-    about_information_frame.grid(column=0, row=1, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W)
-    about_information_frame.configure(fg="#3498db", bg="#434547", bd=3, font=(set_font, 10, "bold"))
+    about_information_frame = LabelFrame(
+        about_window, text=" License ", labelanchor="nw"
+    )
+    about_information_frame.grid(
+        column=0, row=1, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W
+    )
+    about_information_frame.configure(
+        fg=frame_color_fg, bg=frame_color_bg, bd=3, font=(set_font, 10, "bold")
+    )
     about_information_frame.grid_rowconfigure(0, weight=1)
     about_information_frame.grid_columnconfigure(0, weight=1)
 
@@ -97,9 +153,15 @@ def openaboutwindow(main_root_title):
     WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     """
 
-    about_window_license = Text(about_information_frame, background="#434547", foreground="white", relief=FLAT)
-    about_window_license.pack(anchor='center')
+    about_window_license = Text(
+        about_information_frame,
+        background=text_color_bg,
+        foreground=text_color_fg,
+        relief=FLAT,
+    )
+    about_window_license.pack(anchor="center")
     about_window_license.insert(INSERT, license_text)
     about_window_license.configure(state=DISABLED)
+
 
 # -------------------------------------------------------------------------------------------------------- About Window
