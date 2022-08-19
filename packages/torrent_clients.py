48f93b65b037e386973b4f97915c055cd3ebd9cc
jlw4049 <jlw_4049@hotmail.com>
2022-08-18 22:22:11 -0400

    A few changes:
    * Removed some un-needed imports
    * Removed all the CLI code and replaced it with WebUI code only
    * Program quickly tries to login to the WebUI on the client, if it is returned in about 6 seconds, it will assume the program is not running, or one of the parameters are incorrectly added
    * This module returns the status of the torrent added or failed and reports back to the main script

--- packages/torrent_clients.py
+++ packages/torrent_clients.py
@@ -1,8 +1,6 @@
 import qbittorrentapi
 import pathlib
 from configparser import ConfigParser
-from subprocess import Popen as subprocess_Popen
-from subprocess import CREATE_NO_WINDOW
 
 
 class Clients:
@@ -16,57 +14,38 @@ class Clients:
         self.client_config.read("runtime/config.ini")
 
     def qbittorrent(self, encode_file_path, torrent_file_path):
-        """check qBittorrent client injection type"""
-        if self.client_config["qbit_client"]["qbit_injection_type"] == "cli":
-            self.qbittorrent_cli(
-                encode_file_path=encode_file_path, torrent_file_path=torrent_file_path
-            )
-        elif self.client_config["qbit_client"]["qbit_injection_type"] == "webui":
-            self.qbittorrent_webui(
-                encode_file_path=encode_file_path, torrent_file_path=torrent_file_path
-            )
-
-    def qbittorrent_webui(self, encode_file_path, torrent_file_path):
-        # qBittorrent client instance
+        """injection into qBittorrent via webui"""
         qbt_client = qbittorrentapi.Client(
-            host=self.client_config["qbit_url"],
-            port=self.client_config["qbit_port"],
-            username=self.client_config["qbit_user"],
-            password=self.client_config["qbit_pass"],
+            host=self.client_config["qbit_client"]["qbit_url"],
+            port=self.client_config["qbit_client"]["qbit_port"],
+            username=self.client_config["qbit_client"]["qbit_user"],
+            password=self.client_config["qbit_client"]["qbit_password"],
         )
-        # console.print("[bold yellow]Adding and rechecking torrent")
+
+        # do a short time out login session to see if qBittorrent is running
         try:
-            qbt_client.auth_log_in()
-        except qbittorrentapi.LoginFailed:
-            # console.print("[bold red]INCORRECT QBIT LOGIN CREDENTIALS")
-            return
-        qbt_client.torrents_add(
-            torrent_files=torrent_file_path.dump(),
-            save_path=pathlib.Path(encode_file_path).parent,
-            use_auto_torrent_management=False,
-            is_skip_checking=True,
-            content_layout="Original",
-        )
-        qbt_client.torrents_resume(torrent_file_path.infohash)
-        # if client.get('qbit_tag', None) != None:
-        # qbt_client.torrents_add_tags(tags=client.get('qbit_tag'), torrent_hashes=torrent.infohash)
+            qbt_client.auth_log_in(requests_args={"timeout": 0.35})
+        except qbittorrentapi.exceptions.APIConnectionError:
+            return (
+                "Injection failed:\nqBittorrent is most likely not running\n\nIf qBittorrent was running check "
+                "hostname, port, user and password before trying automatic injection again"
+            )
 
-    def qbittorrent_cli(self, encode_file_path, torrent_file_path):
-        """qbittorrent cli injection mode"""
+        # add torrent file to qBittorrent
+        try:
+            add_torrent = qbt_client.torrents_add(
+                torrent_files=torrent_file_path,
+                save_path=pathlib.Path(encode_file_path).parent,
+                use_auto_torrent_management=False,
+                is_skip_checking=True,
+                content_layout="Original",
+            )
+        except qbittorrentapi.exceptions.APIConnectionError:
+            raise ValueError(
+                "There was an error adding torrent file to qBittorrent via WebUI"
+            )
 
-        # check if skip_check is true or false
-        if self.client_config["qbit_client"]["qbit_cli_skip_check"] == "true":
-            skip_hash = "--skip-hash-check "
-        elif self.client_config["qbit_client"]["qbit_cli_skip_check"] == "false":
-            skip_hash = ""
-
-        # create command to be sent to subprocess_Popen
-        cli_command = (
-            f'"{self.client_config["qbit_client"]["qbit_path"]}" --skip-dialog=true --no-splash '
-            f'--save-path="{str(pathlib.Path(encode_file_path).parent)}" '
-            f'{skip_hash}--add-paused={self.client_config["qbit_client"]["qbit_cli_paused"]} '
-            f'"{str(pathlib.Path(torrent_file_path))}"'
-        )
-
-        # run the command with subprocess Popen
-        subprocess_Popen(cli_command, creationflags=CREATE_NO_WINDOW)
+        if add_torrent == "Ok.":
+            return "qBittorrent torrent injection successful!"
+        elif add_torrent == "Fails.":
+            return "qBittorrent torrent injection was not successful"
