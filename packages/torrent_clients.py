9e8277ee7339ddb9651d86edce4dd412d6b6fe96
jlw4049 <jlw_4049@hotmail.com>
2022-08-22 22:03:12 -0400

    Added deluge injection support
    * The module now has full deluge injection support
    * Added the code to check qbittorrent as well

--- packages/torrent_clients.py
+++ packages/torrent_clients.py
@@ -1,10 +1,12 @@
-import qbittorrentapi
-import pathlib
-from configparser import ConfigParser
-from deluge_client import DelugeRPCClient, client
-import deluge_client
-import torf
 import base64
+import pathlib
+import socket
+from configparser import ConfigParser
+
+import deluge_client.client
+import qbittorrentapi
+import torf
+from deluge_client import DelugeRPCClient
 
 
 class Clients:
@@ -26,13 +28,15 @@ class Clients:
             password=self.client_config["qbit_client"]["qbit_password"],
         )
 
-        # do a short time out login session to see if qBittorrent is running
+        # check if qBittorrent login works
         try:
             qbt_client.auth_log_in(requests_args={"timeout": 0.35})
+        except qbittorrentapi.LoginFailed:
+            return "Injection failed...\n\nCheck username and password and try again"
         except qbittorrentapi.exceptions.APIConnectionError:
             return (
-                "Injection failed:\nqBittorrent is most likely not running\n\nIf qBittorrent was running check "
-                "hostname, port, user and password before trying automatic injection again"
+                "qBittorrent is not detected. Ensure that it's running and try again.\n\nIf qBittorrent is "
+                "running check host and port"
             )
 
         # change variables around depending on local vs remote injection
@@ -67,5 +71,150 @@ class Clients:
         elif add_torrent == "Fails.":
             return "qBittorrent torrent injection was not successful"
 
-    def deluge(self):
-        pass
+    @staticmethod
+    def qbittorrent_test(host, port, username, password):
+        """method to check qBittorrent's login settings"""
+        qbt_client = qbittorrentapi.Client(
+            host=host,
+            port=port,
+            username=username,
+            password=password,
+        )
+
+        # check if qBittorrent login works
+        try:
+            qbt_client.auth_log_in(requests_args={"timeout": 0.35})
+            return "Successful!\n\nAs long as category is setup correctly automatic injection will work!"
+        except qbittorrentapi.LoginFailed as e:
+            return "Injection failed...\n\nCheck username and password and try again"
+        except qbittorrentapi.exceptions.APIConnectionError:
+            return (
+                "qBittorrent is not detected. Ensure that it's running and try again.\n\nIf qBittorrent is "
+                "running check host and port"
+            )
+
+    def deluge(self, torrent_file_path):
+        """injection for Deluge via WebUI"""
+        deluge_client_inj = DelugeRPCClient(
+            host=self.client_config["deluge_client"]["deluge_url"],
+            port=int(self.client_config["deluge_client"]["deluge_daemon_port"]),
+            username=self.client_config["deluge_client"]["deluge_user"],
+            password=self.client_config["deluge_client"]["deluge_password"],
+        )
+
+        # connect to deluge client
+        try:
+            deluge_client_inj.connect()
+
+            # if deluge_client does not return connected
+            if not deluge_client_inj.connected:
+                return (
+                    "Failed to connect...\n\nEnsure that you followed all the steps to get it "
+                    "working, if you have done so it's likely your host, port, username, and/or "
+                    "password is incorrect"
+                )
+
+        # error returned if deluge can not be found
+        except socket.timeout:
+            return (
+                "Could not access Deluge. This is likely due to host or port being incorrect, or "
+                "Deluge/Deluge's daemon not running..."
+            )
+
+        # all other errors that deluge returns once connected
+        except deluge_client.client.DelugeClientException as error:
+            error_var = str(error)
+
+            if "username does not exist" in error_var.lower():
+                return "Username is incorrect"
+
+            if "password does not match" in error_var.lower():
+                return "Incorrect password"
+
+        # if connected continue with injection
+        if deluge_client_inj.connected:
+
+            # create torrent instance to get torrent attributes
+            read_torrent = torf.Torrent.read(torrent_file_path)
+
+            # set injected torrent name to a variable
+            injected_torrent_name = read_torrent.name
+
+            # inject torrent name to client with specified settings
+            try:
+                deluge_client_inj.call(
+                    "core.add_torrent_file",
+                    torrent_file_path,
+                    base64.b64encode(read_torrent.dump()),
+                    {
+                        "download_location": self.client_config["deluge_client"][
+                            "deluge_remote_path"
+                        ],
+                        "seed_mode": True,
+                    },
+                )
+
+            # deluge's api is awful, so catch all errors and raise a value error
+            except deluge_client_inj.DelugeClientException as error:
+                return f"Could not inject torrent to client...\nError:\n{error}"
+
+            # parse client for torrent names
+            parse_client = deluge_client_inj.call(
+                "core.get_torrents_status", {}, ["name"]
+            )
+
+            # check if injected torrent is in the torrent names parse_client
+            for x in parse_client.values():
+                name_values = list(x.values())[0].decode("utf-8")
+                if injected_torrent_name == name_values:
+                    return "Successfully injected into Deluge!"
+
+        # if connected fails
+        else:
+            return "Unable to access Deluge client"
+
+    @staticmethod
+    def deluge_test(host, port, username, password):
+        """method to check Deluge login settings"""
+        deluge_client_inj = DelugeRPCClient(
+            host=host,
+            port=int(port),
+            username=username,
+            password=password,
+        )
+
+        # connect to deluge client
+        try:
+            deluge_client_inj.connect()
+
+            # if deluge_client does not return connected
+            if not deluge_client_inj.connected:
+                return (
+                    "Failed to connect...\n\nEnsure that you followed all the steps to get it "
+                    "working, if you have done so it's likely your host, port, username, and/or "
+                    "password is incorrect"
+                )
+
+                # error returned if deluge can not be found
+        except socket.timeout:
+            return (
+                "Could not access Deluge. This is likely due to host or port being incorrect, or "
+                "Deluge/Deluge's daemon not running..."
+            )
+
+            # all other errors that deluge returns once connected
+        except deluge_client.client.DelugeClientException as error:
+            error_var = str(error)
+
+            if "username does not exist" in error_var.lower():
+                return "Username is incorrect"
+
+            if "password does not match" in error_var.lower():
+                return "Incorrect password"
+
+        # if login passes all checks
+        if deluge_client_inj.connected:
+            return (
+                "Connected to client!\n\nAs long as 'Save Directory' is configured correctly "
+                "your torrent will inject automatically"
+            )
