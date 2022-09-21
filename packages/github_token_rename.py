bed32e0c8fed1fb012d8110b84e1384580e802bc
jlw4049 <jlw_4049@hotmail.com>
2022-09-21 19:13:37 -0400

    Create github_token_rename.py

---
+++ packages/github_token_rename.py
@@ -0,0 +1,8 @@
+"""
+This needs a token generated and added here
+You can generate this token "https://github.com/settings/tokens"
+
+Once that is done you need to rename this file from "github_token_rename" to "github_token"
+"""
+
+github_token = ""
