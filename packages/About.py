2d9911b20b6f4a3d7c37d0d7652574440ac690d4
jlw4049 <jlw_4049@hotmail.com>
2022-10-24 09:18:47 -0400

    Update the about window to properly scale to the new UI scale feature

--- packages/About.py
+++ packages/About.py
@@ -15,7 +15,6 @@ from tkinter import (
 )
 
 
-# About Window --------------------------------------------------------------------------------------------------------
 def openaboutwindow(
     main_root_title,
     main_root_color,
@@ -23,6 +22,7 @@ def openaboutwindow(
     frame_color_fg,
     text_color_fg,
     text_color_bg,
+    set_font_size,
 ):
     global about_window
 
@@ -51,52 +51,45 @@ def openaboutwindow(
                 func_parser["save_window_locations"]["about_window"]
                 != about_window.geometry()
             ):
-                if (
-                    int(about_window.geometry().split("x")[0]) >= about_window_width
-                    or int(about_window.geometry().split("x")[1].split("+")[0])
-                    >= about_window_height
-                ):
-                    func_parser.set(
-                        "save_window_locations", "about_window", about_window.geometry()
-                    )
-                    with open(config_file, "w") as configfile:
-                        func_parser.write(configfile)
+                func_parser.set(
+                    "save_window_locations", "about_window", about_window.geometry()
+                )
+                with open(config_file, "w") as configfile:
+                    func_parser.write(configfile)
 
         about_window.destroy()  # Close window
 
     about_window = Toplevel()
     about_window.title("About")
     about_window.configure(background=main_root_color)
-    about_window_height = 650
-    about_window_width = 720
-    if config["save_window_locations"]["about_window"] == "":
-        screen_width = about_window.winfo_screenwidth()
-        screen_height = about_window.winfo_screenheight()
-        x_coordinate = int((screen_width / 2) - (about_window_width / 2))
-        y_coordinate = int((screen_height / 2) - (about_window_height / 2))
-        about_window.geometry(
-            "{}x{}+{}+{}".format(
-                about_window_width, about_window_height, x_coordinate, y_coordinate
-            )
-        )
-    elif config["save_window_locations"]["about_window"] != "":
+    if config["save_window_locations"]["about_window"] != "":
         about_window.geometry(config["save_window_locations"]["about_window"])
     about_window.resizable(False, False)
     about_window.protocol("WM_DELETE_WINDOW", about_exit_function)
 
     about_window.grid_columnconfigure(0, weight=1)
+    about_window.grid_rowconfigure(0, weight=1)
+    about_window.grid_rowconfigure(1, weight=1)
 
     detect_font = font.nametofont(
         "TkDefaultFont"
     )  # Get default font value into Font object
     set_font = detect_font.actual().get("family")
 
-    about_information_frame = LabelFrame(about_window, text=" About ", labelanchor="nw")
+    about_information_frame = LabelFrame(
+        about_window,
+        text=" About ",
+        labelanchor="nw",
+        font=(set_font, set_font_size + 1, "bold"),
+    )
     about_information_frame.grid(
-        column=0, row=0, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W
+        column=0, row=0, padx=5, pady=(0, 3), sticky=N + S + E + W
     )
     about_information_frame.configure(
-        fg=frame_color_fg, bg=frame_color_bg, bd=3, font=(set_font, 10, "bold")
+        fg=frame_color_fg,
+        bg=frame_color_bg,
+        bd=3,
+        font=(set_font, set_font_size, "bold"),
     )
     about_information_frame.grid_rowconfigure(0, weight=1)
     about_information_frame.grid_columnconfigure(0, weight=1)
@@ -107,8 +100,9 @@ def openaboutwindow(
         foreground=text_color_fg,
         relief=FLAT,
         height=10,
+        font=(set_font, set_font_size - 2),
     )
-    about_window_text.pack()
+    about_window_text.grid(column=0, row=0, sticky=N + S + E + W)
     about_window_text.insert(INSERT, f"{main_root_title}\n")
     about_window_text.insert(INSERT, "\n")
     about_window_text.insert(
@@ -119,17 +113,23 @@ def openaboutwindow(
     )
     about_window_text.configure(state=DISABLED)
 
-    about_information_frame = LabelFrame(
-        about_window, text=" License ", labelanchor="nw"
+    about_information_frame2 = LabelFrame(
+        about_window,
+        text=" License ",
+        labelanchor="nw",
+        font=(set_font, set_font_size + 1, "bold"),
     )
-    about_information_frame.grid(
-        column=0, row=1, columnspan=1, padx=5, pady=(0, 3), sticky=N + S + E + W
+    about_information_frame2.grid(
+        column=0, row=1, padx=5, pady=(0, 10), sticky=N + S + E + W
     )
-    about_information_frame.configure(
-        fg=frame_color_fg, bg=frame_color_bg, bd=3, font=(set_font, 10, "bold")
+    about_information_frame2.configure(
+        fg=frame_color_fg,
+        bg=frame_color_bg,
+        bd=3,
+        font=(set_font, set_font_size, "bold"),
     )
-    about_information_frame.grid_rowconfigure(0, weight=1)
-    about_information_frame.grid_columnconfigure(0, weight=1)
+    about_information_frame2.grid_rowconfigure(0, weight=1)
+    about_information_frame2.grid_columnconfigure(0, weight=1)
 
     license_text = """
     Copyright (c) 2012-2022 Scott Chacon and others
@@ -155,12 +155,14 @@ def openaboutwindow(
     """
 
     about_window_license = Text(
-        about_information_frame,
+        about_information_frame2,
         background=text_color_bg,
         foreground=text_color_fg,
         relief=FLAT,
+        height=22,
+        font=(set_font, set_font_size - 2),
     )
-    about_window_license.pack(anchor="center")
+    about_window_license.grid(column=0, row=0, sticky=N + S + E + W)
     about_window_license.insert(INSERT, license_text)
     about_window_license.configure(state=DISABLED)
 
