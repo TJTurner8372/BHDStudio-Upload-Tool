ccf802ad7a9a5d0cdb33db0a1120ec7248fa2ae1
jlw4049 <jlw_4049@hotmail.com>
2022-12-08 15:34:50 -0500

    Clean up code and add filter for resolution
    - Added the ability to filter by resolution for each title
    - Increase timeout to 60 seconds per request, in the event the site is running sloooow
    - Added if __name__ main to the bottom with an example

--- packages/dupe_checker.py
+++ packages/dupe_checker.py
@@ -1,13 +1,14 @@
 import requests
 
 
-def dupe_check(api_key: str, title: str, page: int = 0):
+def dupe_check(api_key: str, title: str, page: int = 0, resolution: str = None):
     """
     Checks BeyondHD for duplicate titles.
 
     :param api_key: API key from BeyondHD.
     :param title: Title of the movie to check.
     :param page: This returns 100 results by default, if for some reason there is more send a page number.
+    :param resolution: Filters results by resolution. If left as None then it will return all results.
     :return: List with a dictionary in it of the first 100 results.
     """
 
@@ -25,10 +26,32 @@ def dupe_check(api_key: str, title: str, page: int = 0):
         payload.update({"page": int(page)})
 
     # post to send the payload
-    run_check = requests.post(
-        "https://beyond-hd.me/api/torrents/" + api_key, params=payload
-    )
+    try:
+        run_check = requests.post(
+            "https://beyond-hd.me/api/torrents/" + api_key, params=payload, timeout=60
+        )
+    except requests.exceptions.ConnectionError:
+        return "Connection Error"
 
-    # if all 3 requirements are correctly returned, continue
+    # release dict
+    release_dict = {}
+
+    # if all 3 checks are successful, return all results in a list.
     if run_check.ok and run_check.json()["status_code"] and run_check.json()["success"]:
-        return [x for x in run_check.json()["results"]]
+        for x in run_check.json()["results"]:
+            if resolution:
+                if resolution in str(x):
+                    release_dict.update({x["name"]: x})
+            elif not resolution:
+                release_dict.update({x["name"]: x})
+
+    return release_dict
+
+
+if __name__ == "__main__":
+    wow = dupe_check(api_key="KEYHERE", title="Gone In 60 Seconds")
+
+    if wow and wow != "Connection Error":
+        print(wow)
+    else:
+        print("Connection Error")
